7c29932f36b05e3a423f5d33178a3f6bc4e60389
Huanyu Li <huanyu.li@liu.se>
2024-03-28 21:05:09 +0100

    restructure

--- build_doc.py
+++ build_doc.py
@@ -2,16 +2,14 @@ from pylode.profiles.vocpub import VocPub
 import os
 from bs4 import BeautifulSoup, formatter
 
-source_ontology_path = './semiconto.ttl'
-onto_json_path = './semiconto.json'
-html_path = './docs/index.html'
-webvowl_json_path = './docs/webvowl/data/semiconto.json'
+source_ontology_path = './ontology/0.1/SemicONTO.ttl'
+onto_json_path = './SemicONTO.json'
+html_path = './docs/0.1/index.html'
+webvowl_json_path = './docs/0.1/webvowl/data/SemicONTO.json'
 
 def generate_webvowl():
     cmd = "java --add-opens java.base/java.lang=ALL-UNNAMED -jar owl2vowl.jar -file {onto} -output {output}".format(onto=source_ontology_path, output=webvowl_json_path)
     os.system(cmd)
-    #mv_cmd = "mv -f {onto_json} {output}".format(onto_json=onto_json_path, output=webvowl_json_path)
-    #os.system(mv_cmd)
 
 def generate_html():
     onto_doc = VocPub(ontology=source_ontology_path)
@@ -20,12 +18,45 @@ def generate_html():
 def add_webvowl_to_html():
     with open(html_path, encoding="utf-8") as f:
         soup = BeautifulSoup(f, "html.parser")
+
+        download_overview = BeautifulSoup(
+            f"""
+            <div class="section" id="download">
+                <dl>
+                    <div>
+                        <dt>
+                            <strong>
+                                Download Serialization
+                            </strong>
+                        </dt>
+                        <dd>
+                            <span><a style="text-decoration:none" href="SemicONTO.ttl" target="_blank"><img src="https://img.shields.io/badge/Format-TTL-blue.svg" alt="TTL"></a></span>
+                            <span><a style="text-decoration:none" href="SemicONTO.owl" target="_blank"><img src="https://img.shields.io/badge/Format-OWL-blue.svg" alt="OWL"></a></span>
+                            <span><a style="text-decoration:none" href="SemicONTO.rdf" target="_blank"><img src="https://img.shields.io/badge/Format-RDF/XML-blue.svg" alt="RDF/XML"></a></span>
+                            <span><a style="text-decoration:none" href="SemicONTO.jsonld" target="_blank"><img src="https://img.shields.io/badge/Format-JSON_LD-blue.svg" alt="JSON-LD"></a></span>
+
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+            <div class="section" id="overview">
+                    <h2>Overview</h2>
+                    <div class="figure">
+                        <iframe width="100%" height ="800px" src="webvowl/index.html#SemicONTO"></iframe>
+                        <div class="caption">
+                            <strong>Figure 1:</strong>
+                                Ontology Overview
+                        </div>
+                    </div>
+                    </div>
+            """, "html.parser")
+
         overview = BeautifulSoup(
              f"""
                <div class="section" id="overview">
                     <h2>Overview</h2>
                     <div class="figure">
-                        <iframe width="100%" height ="800px" src="webvowl/index.html#semiconto"></iframe>
+                        <iframe width="100%" height ="800px" src="webvowl/index.html#SemicONTO"></iframe>
                         <div class="caption">
                             <strong>Figure 1:</strong>
                                 Ontology Overview
@@ -34,8 +65,9 @@ def add_webvowl_to_html():
                     </div>
                 """, "html.parser")
         tag = soup.find(id='classes')
-        tag.insert_before(overview)
+        tag.insert_before(download_overview)
         html_formatter = formatter.HTMLFormatter(indent=4)
+
         with open(html_path, "w") as f:
             f.write(soup.prettify(formatter=html_formatter))
 
